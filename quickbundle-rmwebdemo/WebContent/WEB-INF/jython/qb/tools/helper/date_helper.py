d7ddcaeae4fbfd0b0818148269831af57648b5a0
mike bai <lostitle@163.com>
2013-04-10 11:15:51 +0800

    move rmwebdemo

--- quickbundle-rmwebdemo/WebContent/WEB-INF/jython/qb/tools/helper/date_helper.py
+++
@@ -1,9 +0,0 @@
-from time import *
-
-def get_datetime():
-    myTime = strftime('%Y-%m-%d %H:%M:%S', localtime())
-    return myTime
-
-def get_datetime_number():
-    myTime = strftime('%Y%m%d%H%M%S', localtime())
-    return myTime
--- quickbundle-rmwebdemo/WebContent/WEB-INF/jython/qb/tools/pub/rm/pub_gc.py
+++
@@ -1,6 +0,0 @@
-'''
-Created on 2011-5-13
-
-@author: qb
-'''
-print '123'
