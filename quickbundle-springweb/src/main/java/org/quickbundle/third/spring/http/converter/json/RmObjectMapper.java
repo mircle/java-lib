1029f0f367f11993a3aefa805b3169a509e5527e
mike bai <lostitle@163.com>
2013-05-06 19:11:00 +0800

    jackson

---
+++ quickbundle-springweb/src/main/java/org/quickbundle/third/spring/http/converter/json/RmObjectMapper.java
@@ -0,0 +1,24 @@
+package org.quickbundle.third.spring.http.converter.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class RmObjectMapper extends ObjectMapper {
+	private static final long serialVersionUID = 1L;
+	
+	boolean writeNumbersAsStrings = false;
+	
+	public boolean isWriteNumbersAsStrings() {
+		return writeNumbersAsStrings;
+	}
+
+
+	public void setWriteNumbersAsStrings(boolean writeNumbersAsStrings) {
+		this.writeNumbersAsStrings = writeNumbersAsStrings;
+		super.configure(JsonGenerator.Feature.WRITE_NUMBERS_AS_STRINGS, writeNumbersAsStrings);
+	}
+
+	public RmObjectMapper() {
+		super();
+	}
+}
